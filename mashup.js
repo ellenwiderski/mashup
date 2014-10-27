465b3a3575a8327aad92946ad8d8e581b9e41187
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-10-27 14:31:38 -0500

    testing

--- mashup.js
+++ mashup.js
@@ -1,3 +1,15 @@
+// GOOGLE MAPS
+
+function initialize() {
+  var mapOptions = {
+    center: { lat: -34.397, lng: 150.644},
+    zoom: 8
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
 //FACEBOOK
 
 // This is called with the results from from FB.getLoginStatus().
@@ -102,16 +114,3 @@ function testAPI() {
       'Thanks for logging in, ' + response.name + '!';
   });
 }
-
-
-// GOOGLE MAPS
-
-function initialize() {
-  var mapOptions = {
-    center: { lat: -34.397, lng: 150.644},
-    zoom: 8
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
