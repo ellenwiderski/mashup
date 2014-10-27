2e28c40f8c5e435222d2b66699acc38c97c36113
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-10-27 16:03:56 -0500

    testing

--- mashup.js
+++ mashup.js
@@ -6,7 +6,7 @@ function initialize() {
     center: { lat: 43.076073222273, lng: -89.376661651794},
     zoom: 8
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+  map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
