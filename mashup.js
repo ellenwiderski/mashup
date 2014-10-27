76a48a356fbeed2585b7a1b1af59cdfaf1cb7d9a
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-10-27 16:27:55 -0500

    testing

--- mashup.js
+++ mashup.js
@@ -133,9 +133,8 @@ function initialize() {
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-  }
-
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
 function loadMarkers(markers) {
