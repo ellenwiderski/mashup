03fb030c2c4c7afbc3dd1457223f553bae0a365a
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-10-27 16:30:03 -0500

    testing

--- mashup.js
+++ mashup.js
@@ -133,6 +133,8 @@ function initialize() {
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+  
+  var bounds = new google.maps.LatLngBounds();
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
