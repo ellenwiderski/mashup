2861773554bab8c1155ad5b92c9685dd024bc234
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-10-27 21:47:57 -0500

    testing

--- mashup.js
+++ mashup.js
@@ -164,29 +164,29 @@ function loadMarkers(markers) {
           '<h3>'+markers[i].friendname+'</h3>' + '</div>');
     }
   }
-}
 
 // Loop through our array of markers & place each one on the map  
 for( i = 0; i < markers.length; i++ ) {
 
-  var position = new google.maps.LatLng(markers[i].lat, markers[i].lng);
-  bounds.extend(position);
+    var position = new google.maps.LatLng(markers[i].lat, markers[i].lng);
+    bounds.extend(position);
 
-  marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: markers[i].placename
-  });
-  
-  // Allow each marker to have an info window    
+    marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: markers[i].placename
+    });
+    
+    // Allow each marker to have an info window    
 
-  google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-            infoWindow.setContent(infoWindowContent[i]);
-            infoWindow.open(map, marker);
-        }
-    })(marker, i));
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+              infoWindow.setContent(infoWindowContent[i]);
+              infoWindow.open(map, marker);
+          }
+      })(marker, i));
 
-  // Automatically center the map fitting all markers on the screen
-  map.fitBounds(bounds);
-}
+    // Automatically center the map fitting all markers on the screen
+    map.fitBounds(bounds);
+  }
+} 
