4be26072990356de0ede3791c67aef86cfd88ad8
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-10-28 09:17:26 -0500

    testing

--- mashup.js
+++ mashup.js
@@ -146,7 +146,6 @@ function loadMarkers(markers) {
   for (i = 0; i < markers.length; i++) {
 
     var str = '<div class="info_content">' +
-            'a href='
             '<img src="https://graph.facebook.com/'+markers[i].friendid+'/picture" />'+
             '<h3>'+markers[i].friendname+'</h3>' +
             '<p>' + markers[i].message + '</p>'
