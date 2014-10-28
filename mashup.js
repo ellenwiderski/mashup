60a293e3cca2a0571831cf428e511bdb83f752d4
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-10-27 22:10:38 -0500

    testing

--- mashup.js
+++ mashup.js
@@ -12,7 +12,7 @@ function statusChangeCallback(response) {
     // Logged into your app and Facebook.
     testAPI();
 
-    FB.api("/me/home?limit=15",
+    FB.api("/me/home?limit=20",
             {
               "with": "location"
           },
